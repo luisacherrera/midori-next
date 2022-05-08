754cc7c749b08323f5ba7cf75ffbd540e28e2673
Luis Acedo Herrera <la.herrera457@gmail.com>
2022-05-08 23:09:13 +0200

    finish homepage styles

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,15 +4,23 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.scss'
 
 const Home: NextPage = () => {
-  const imageSource = `/photos/biblioteca-arenys/Concurs Biblioteca d'Arenys de Mar (finalista).jpg`
+  const imageSource = `/photos/biblioteca-arenys/Concurs Biblioteca d'Arenys de Mar (finalista).jpg`;
+  const projectName = `Concurs Biblioteca d'Arenys de Mar (finalista)`
 
   return (
-    <div className={styles.wrapper}>
+    <>
+      <div className={styles.logo_backdrop}></div>
+      <div className={styles.logo_container}>
+        <Image className={styles.logo_container_img} src='/logos/midori_logomaster.png' alt='midori logo' layout='fill'/>
+      </div>
+      <div className={styles.title_container}>
+        <p className={styles.title_container_text}>{projectName}</p>
+      </div>
       <div className={styles.image_container}>
         <Image className={styles.image_container_img} src={imageSource} alt='project image' layout='fill'/>
       </div>
       <Footer></Footer>
-    </div>
+    </>
   )
 }
 
