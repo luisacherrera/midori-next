2d7e5323243a861bb847ca8a791f626dca77d146
Luis Acedo Herrera <la.herrera457@gmail.com>
2022-05-08 22:18:07 +0200

    adjust footer, add background image in home

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,16 @@
 import type { NextPage } from 'next'
 import Footer from '../components/Footer'
+import Image from 'next/image'
 import styles from '../styles/Home.module.scss'
 
 const Home: NextPage = () => {
+  const imageSource = `/photos/biblioteca-arenys/Concurs Biblioteca d'Arenys de Mar (finalista).jpg`
+
   return (
-    <div>
+    <div className={styles.wrapper}>
+      <div className={styles.image_container}>
+        <Image className={styles.image_container_img} src={imageSource} alt='project image' layout='fill'/>
+      </div>
       <Footer></Footer>
     </div>
   )
